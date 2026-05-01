ce837fccd825a3a70ca21179f181f83dc14bb685
Aloko Mayowa Daniel <mayowaaloko@gmail.com>
2026-05-01 19:05:13 +0100

    prisma configurated completed

---
+++ src/config/db.ts
@@ -0,0 +1,43 @@
+import "dotenv/config";
+import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaNeon } from "@prisma/adapter-neon";
+
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("❌ DATABASE_URL is not defined in .env");
+}
+
+const adapter = new PrismaNeon({
+  connectionString,
+});
+
+export const prisma = new PrismaClient({
+  adapter,
+  log:
+    process.env.NODE_ENV === "development"
+      ? ["query", "info", "warn", "error"]
+      : ["warn", "error"],
+});
+const connectDB = async () => {
+  try {
+    await prisma.$connect();
+    console.log("✅ Database connected successfully via Prisma");
+  } catch (error: unknown) {
+    const message = error instanceof Error ? error.message : String(error);
+    console.error(`❌ Database connection failed: ${message}`);
+    process.exit(1);
+  }
+};
+
+const disconnectDB = async () => {
+  try {
+    await prisma.$disconnect();
+    console.log("✅ Database disconnected successfully via Prisma");
+  } catch (error: unknown) {
+    const message = error instanceof Error ? error.message : String(error);
+    console.error(`❌ Database disconnection failed: ${message}`);
+  }
+};
+
+export default { prisma, connectDB, disconnectDB };
